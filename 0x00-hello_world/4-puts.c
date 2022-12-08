8d0261c2fbeba49733142073350e902391853ccb
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 <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>
2022-12-08 12:12:59 -0800

    fixed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[])
-{
-	char str[]="\"Programming is like building a multilingual puzzle";
+int main(){
+	char str[]="\"Programming is like building a multilingual puzzle\n";
 	puts(str);
 	return (0);
 }
