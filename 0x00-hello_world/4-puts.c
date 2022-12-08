a5a90b9f309d655aa0a8706e336772e34ee5cfcf
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 <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>
2022-12-08 11:56:05 -0800

    C program that print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
-	char str="\"Programming is like building a multilingual puzzle";
+	char str[]="\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
 }
