fd8df2bd5e378a0185943c2d66022e3679450f15
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 <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>
2022-12-09 04:46:08 -0800

    done with task 1

---
+++ 0x00-hello_world/testcode.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints onto screen
+ *
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+	/* CODE GOES HERE */
+	return(0);
+}
