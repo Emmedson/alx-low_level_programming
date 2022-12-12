dd7b1908d7db2ede2ac6febe8538ef09405fbe50
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 <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>
2022-12-12 13:58:00 -0800

    output string with write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+ *main - write funtion to output string 
+ *
+ *Return: 1 after execution 
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+	return (1);
+}
