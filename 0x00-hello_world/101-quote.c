1da512011e36a11e2a494a94d8bd860fd9a7daf4
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 <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>
2022-12-12 14:17:35 -0800

    fix betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
- *main - write funtion to output string 
- *
- *Return: 1 after execution 
- */
+/**
+ * main - write funtion to output string
+ * Return: 1 after execution
+*/
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
