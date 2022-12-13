55fdadc672996abe908c86007bfd7832f0fdd1d3
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 <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>
2022-12-13 13:58:19 -0800

    done task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - function to print _putchar
+ *
+ *
+ *Return: zero or number value
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
