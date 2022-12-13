b1221e0e586ef014c99857dcc512de536903a39c
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 <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>
2022-12-13 15:21:22 -0800

    done task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - function to print alphabet
+ *
+ *
+ *Return: zero or number value
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++){
+	_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
