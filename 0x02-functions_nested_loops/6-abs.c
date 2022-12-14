a14183774c437c780840b328405e67b18fac4efb
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 <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>
2022-12-13 17:34:23 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function to print sign of a number
+ *
+ * @int: character to be checked
+ *Return: zero or number value
+ */
+
+int _abs(int)
+{
+
+	if (n < 0)
+	{
+		n = n * (-1);
+	}
+
+	return (0);
+
+}
