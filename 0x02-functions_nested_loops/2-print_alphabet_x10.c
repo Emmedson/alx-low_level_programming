565862f43c5603e10f3b80b6144947950645af8a
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 <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>
2022-12-13 16:12:27 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print alphabet 10times
+ *
+ *
+ *Return: zero or number value
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	int x;
+
+	for ( = '0'; i <= '9'; i++)
+	{
+		for (x= 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+	}
+
+	_putchar('\n');
+}
