1a3d2ac32519ec33140dbff23964cbf0cf2085df
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 <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>
2022-12-19 09:29:58 -0800

    reset memory value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset memory alocation value to 98
+ * @n: Pointer to n number
+ * Return: Zero 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
