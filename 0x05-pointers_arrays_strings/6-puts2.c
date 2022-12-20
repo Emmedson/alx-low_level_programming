5abd04020be6c56010335ee73335cc2fc94f5099
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 <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>
2022-12-19 20:23:22 -0800

    print string character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0' && i % 2 == 0)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
