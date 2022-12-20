834e72592971283d370dce0a47dd3636cd6716b9
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 <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>
2022-12-19 21:06:57 -0800

    fix bug

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,33 +1,21 @@
-i#include "main.h"
+#include "main.h"
 
 /**
- * _strcpy - A function that print half of string
+ * _strcpy - A function to copy string pointer
  *
- * @a: array int pointer
- * @n: array variable
- * Return: 0
+ * @dest: string pointer
+ * @src: string pointer
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
-
 {
-
-		int i;
-
-
-
-			for (i = 0; *(src + i) != '\0'; i++)
-
-					{
-
-								dest[i] = *(src + i);
-
-									}
-
-				dest[i] = '\0';
-
-
-
-					return (dest);
-
+	int i;
+	
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+	return (dest);
 }
