7b5176895f337205ac5015276abde3ef7b7ccda5
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 <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>
2022-12-22 10:06:55 -0800

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncmp - function to  compare two strings
+ *
+ *@s1: pointer to destination string
+ *@s2: pointer to source string
+ *@n: number of character to be concatenated
+ *Return: pointer to destination string
+ */
+
+int _strcmp(char *s1, char *s2 int n)
+{
+	strncmp(s1, s2, n);
+	return (0);
+}
